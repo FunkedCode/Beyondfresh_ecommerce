d090b1deb525bdeb2c3b28b7a7c8416bb49767f8
GhileMear <sorenff@gmail.com>
2019-03-18 19:52:14 -0500

    basic view for products made

--- app/views/products/_product.json.jbuilder
+++ app/views/products/_product.json.jbuilder
@@ -1,2 +1,4 @@
-json.extract! product, :id, :title, :price, :description, :image, :serving_size, :ingredient_list, :order_product_id, :product_attribute_id, :created_at, :updated_at
+# frozen_string_literal: true
+
+json.extract! product, :id, :title, :price, :description, :image, :serving_size, :ingredient_list, :product_category_ids, :created_at, :updated_at
 json.url product_url(product, format: :json)
--- config/application.rb
+++ config/application.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require_relative 'boot'
 
 require 'rails/all'
@@ -15,5 +17,8 @@ module Beyondfresh
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.generators do |g|
+      g.template_engine :erb
+    end
   end
 end
