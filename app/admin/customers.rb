22059701b601a61c6ad37a57c00193a0d6473b83
GhileMear <sorenff@gmail.com>
2019-03-19 16:36:30 -0500

    Permitted parameters to active admin for crud actions

---
+++ app/admin/customers.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+ActiveAdmin.register Customer do
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  permit_params :first_name, :last_name, :user_name, :id
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:permitted, :attributes]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+end
