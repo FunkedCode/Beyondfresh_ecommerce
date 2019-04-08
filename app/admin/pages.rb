e00028e72e5ac0ca3460aa11ba7f298bdcc775df
GhileMear <sorenff@gmail.com>
2019-04-08 16:18:36 -0500

    Added image upload to pages

--- app/admin/pages.rb
+++ app/admin/pages.rb
@@ -4,7 +4,7 @@ ActiveAdmin.register Page do
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
-  permit_params :body, :sub_content, :header, :page_name
+  permit_params :body, :sub_content, :header, :page_name, :image
   #
   # or
   #
@@ -13,4 +13,5 @@ ActiveAdmin.register Page do
   #   permitted << :other if params[:action] == 'create' && current_user.admin?
   #   permitted
   # end
+  form partial: 'form'
 end
